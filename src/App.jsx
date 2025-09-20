dfdbee66dba20075d97306ed3c5226dd710725fb
mohammademon393 <mdemon357ep@gmail.com>
2025-09-20 21:49:50 +0600

    create json data and load

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 
 import './App.css'
 
+const bottlePromies = fetch('./bottle.json').then(res => res.json()).then(data => console.log(data))
+console.log(bottlePromies);
+
+
 function App() {
 
   return (
