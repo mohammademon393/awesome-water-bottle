8c359b94c1b9141b248a212b4b3754d39d89bc3e
mohammademon393 <mdemon357ep@gmail.com>
2025-09-20 22:38:50 +0600

    Two css components create

--- src/Component/Bottle/Bottle.jsx
+++ src/Component/Bottle/Bottle.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const Bottle = () => {
-    return (
-        <div>
-            <h2>bottleeee</h2>
-        </div>
-    );
+const Bottle = ({ bottle }) => {
+    
+  return (
+    <div>
+      <img src={bottle.image} alt="" />
+      <h2>Name: {bottle.name}</h2>
+      <p>Brand: {bottle.brand}</p>
+      <h6>Material: {bottle.material}</h6>
+      <p>Capacity_ml: {bottle.capacity_ml}</p>
+    </div>
+  );
 };
 
 export default Bottle;
--- src/Component/Bottles/Bottles.jsx
+++ src/Component/Bottles/Bottles.jsx
@@ -1,12 +1,16 @@
 import React, { use } from 'react';
+import Bottle from '../Bottle/Bottle';
 
 const Bottles = ({ bottlePromies }) => {
     const bottles = use(bottlePromies);
-    console.log(bottles);
+    // console.log(bottles);
     
   return (
     <div>
-      <h2>bottlessss</h2>
+      <h2>Bottles: {bottles.length}</h2>
+      {
+        bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
+      }
     </div>
   );
 };
